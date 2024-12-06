a7efbb1e72a7616a3e84f11753eb336299618dc3
ledsouza <leandro.souza.159@gmail.com>
2024-12-06 18:01:52 -0300

    feat: added help and exit commands to cli

---
+++ repl.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(" >")
+		scanner.Scan()
+
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		inputCommand := cleaned[0]
+
+		commands := getCommands()
+		command, ok := commands[inputCommand]
+		if !ok {
+			fmt.Println("invalid command")
+			continue
+		}
+		command.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
